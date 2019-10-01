0a5d6006475e0dfea49e79952b3b95aff5276a48
fuzzylogicxx <laurence@fuzzylogicdigital.com>
2019-10-01 16:55:02 +0100

    Getting exchange rates using net/http

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -1,5 +1,15 @@
 class RatesController < ApplicationController
 
+    require 'net/http'
+    require 'json'
+
+    def index
+        uri = URI('http://data.fixer.io/api/latest?access_key=429ca83b5757bee8cbf4b997f620f3ab&base=EUR&symbols=USD,GBP,JPY,AUD,CAD,CHF,CNY,SEK,NZD')
+        response = Net::HTTP.get(uri)
+        @rates = JSON.parse(response)
+    end
+
+
     private
         def rate_params
             params.require(:rate).permit(:amount)
