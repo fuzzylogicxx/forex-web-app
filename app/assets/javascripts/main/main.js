403e42861610fc79b08292a087ca0a2011956edf
fuzzylogicxx <laurence@fuzzylogicdigital.com>
2019-10-02 23:55:05 +0100

    Latest Rates table now updates on form submit

--- app/assets/javascripts/main/main.js
+++ app/assets/javascripts/main/main.js
@@ -15,21 +15,22 @@
 
     const amountInput = document.querySelector('#amount');
     let baseValue = 100;
-
     let selectedCurrencies = [];
-
-    let comparisonTableMap = [];
 
     const modal = document.querySelector('#modal');
     const modalOverlay = document.querySelector('#modal-overlay');
     const modalBody = document.querySelector('#modal-body');
 
 
-
     //
     // Functions
     //
 
+    // Format a calculated rate to two decimal places
+    const formatRate = (value) => value.toFixed(2);
+
+    
+    // Asynchronously fetch from fixer.io’s Historical Rates Endpoint
     async function getRatesForDay(day) {
         const url = `${fixerUrl}/${day}?access_key=${fixerAccessKey}&base=${baseCurrency}&symbols=${otherCurrencies.join(',')}`;
 
@@ -43,11 +44,102 @@
         }
     }
 
-    // Let’s not attempt to do any JS enhancement unless we’ve got all the necessary data. 
-    // It’s OK, because if data fetching fails, we’ve still got a reasonable non-JS baseline.
-    // Promise.all() is a good match here:
-    // It is rejected overall if any of the elements are rejected. 
-    // Also, Promise.all()’s returned value order matches the order of the Promises passed, regardless of completion order.
+
+    // Accept a new Euro base value and re-render the Latest Rates table
+    const updateLatestRatesTable = (newBaseValue) => {
+        if (!newBaseValue || (newBaseValue.length === 0) || (newBaseValue === baseValue)) return;
+
+        baseValue = newBaseValue;
+
+        const latestRatesContainer = document.querySelector('#latest-rates-table-container');
+        const latestRatesTable = document.querySelector('#latest-rates-table');
+        if (!latestRatesTable) return;
+
+        const tHeadMarkup = `<tr><th>Currency</th><th>Amount</th></tr>`;
+        const tBodyMarkup = Object.entries(dailyRates[0].rates).map(([currencyName, rate]) => {
+            return `<tr data-currency="${currencyName}"><td>${currencyName}</td><td>${formatRate(rate * baseValue)}</td></tr>`;
+        }).join('');
+        const newTableHTML = '<table id="latest-rates-table"><thead>' + tHeadMarkup + '</thead><tbody>' + tBodyMarkup + '</tbody></table>';
+        latestRatesTable.remove();
+        latestRatesContainer.innerHTML = newTableHTML;
+    };
+
+    
+    // Mark a currency as selected for comparison
+    // and if its the second of a pair, launch the comparison tool.
+    const selectCurrencyForComparison = (currencyRow) => {
+        const currency = currencyRow.getAttribute('data-currency');
+        
+        // if the currency is already selected, quit here.
+        if (selectedCurrencies.indexOf(currency) !== -1) return;
+
+        currencyRow.classList.add('currency-selected');
+
+        selectedCurrencies.push(currency);
+
+        if (selectedCurrencies.length === 2) {
+            launchComparisonTool();
+        }
+    };
+
+    // Clear Selected Currencies
+    const clearSelectedCurrencies = () => {
+        let selectedRows = document.querySelectorAll('.currency-selected');
+        selectedRows.forEach((selectedRow) => selectedRow.classList.remove('currency-selected'));
+        
+        selectedCurrencies = [];
+    };
+
+    
+    // Launch a 2-currency, 5-day rate comparison tool.
+    const launchComparisonTool = () => {
+        setTimeout(() => {
+            modal.classList.toggle("closed");
+            modalOverlay.classList.toggle("closed");
+        }, 500);
+
+        const preTableHeader = `<h2>EUR: ${baseValue}</h2><h3>${selectedCurrencies[0]} and ${selectedCurrencies[1]}: last 5 days</h3>`;
+        const tHeadMarkup = `<tr><th>Date</th><th>${selectedCurrencies[0]}</th><th>${selectedCurrencies[1]}</th></tr>`;
+        const tBodyMarkup = dailyRates.map((row) => {
+            return `<tr><td>${row.date}</td><td>${formatRate(row.rates[selectedCurrencies[0]])}</td><td>${formatRate(row.rates[selectedCurrencies[1]])}</td></tr>`;
+        }).join('');
+        const tableHTML = preTableHeader + '<table><thead>' + tHeadMarkup + '</thead><tbody>' + tBodyMarkup + '</tbody></table>';
+        modalBody.innerHTML = tableHTML;
+    };
+
+ 
+    // Handle form submit events
+    const submitHandler = (e) => {
+        e.preventDefault();
+        if (!amountInput) return;
+        updateLatestRatesTable(amountInput.value);
+    };
+
+
+    // Handle click events
+    const clickHandler = (e) => {
+
+        // Currency row clicks
+        if (e.target.matches('td')) {
+            const currencyRow = e.target.parentNode;
+            selectCurrencyForComparison(currencyRow);
+            return;
+        }
+
+        // 'Close modal' clicks
+        if (e.target.matches('#close-button')) {
+            clearSelectedCurrencies();
+            modal.classList.toggle("closed");
+            modalOverlay.classList.toggle("closed");
+            return;
+        }
+    };
+
+    
+    // Set up: 
+    // Only attempt JS-based enhancements if we managed to fetch data for ALL days. 
+    // We can regard the JS features as enhancements because we already have a decent, non-JS-reliant baseline.
+    // Promise.all() is a good fit for our needs. It rejects overall if any individual promise is rejected. 
 
     var stubPromise = new Promise(function(resolve, reject) {
         setTimeout(function() {
@@ -73,112 +165,13 @@
 
         // Add (to start of array) today’s rates, which were already provided server-side.
         dailyRates.unshift({'date': `${today}`, 'rates': ratesToday});
-        //console.log(dailyRates);
+
+        // Finally, listen for events
+        document.addEventListener('submit', submitHandler, false);
+        document.addEventListener('click', clickHandler, false);
     })
     .catch(error => { 
       console.error(error.message)
     });
-
-
-    
-    
-    // When 'euro_amount' changed, update the 'Latest Exchange Rate' table
-    // Build complete markup first (tbody), THEN inject into the DOM (rather than bit-by-bit, because the latter causes janky performance)
-
-    // When two rows selected, create a 5-day-overview table for the two selected currencies
-    // showing rate*euro_amount for each value
-    // and launch it in a modal
-
-    const updateLatestRatesTable = (baseValue) => {
-        console.log('updateLatestRatesTable', baseValue);
-    };
-
-    
-    const selectCurrencyForComparison = (currencyRow) => {
-        const currency = currencyRow.getAttribute('data-currency');
-        
-        // if the currency is already selected, quit here.
-        if (selectedCurrencies.indexOf(currency) !== -1) return;
-
-        // add class to table row to appear selected
-        currencyRow.classList.add('currency-selected');
-
-        // update our array of selectedCurrencies
-        selectedCurrencies.push(currency);
-
-        if (selectedCurrencies.length === 2) {
-            launchComparisonTool();
-        }
-    };
-
-    
-    const clearSelectedCurrencies = () => {
-        let selectedRows = document.querySelectorAll('.currency-selected');
-        selectedRows.forEach((selectedRow) => selectedRow.classList.remove('currency-selected'));
-        
-        selectedCurrencies = [];
-    };
-
-    
-    const launchComparisonTool = () => {
-        
-        setTimeout(() => {
-            modal.classList.toggle("closed");
-            modalOverlay.classList.toggle("closed");
-        }, 500);
-
-        const tableHeader = `<h2>EUR: ${baseValue}</h2><h3>${selectedCurrencies[0]} and ${selectedCurrencies[1]}: last 5 days</h3>`;
-        const tHeadMarkup = `<tr><td>Date</td><td>${selectedCurrencies[0]}</td><td>${selectedCurrencies[1]}</td></tr>`;
-        const tBodyMarkup = dailyRates.map((row) => {
-            return `<tr><td>${row.date}</td><td>${row.rates[selectedCurrencies[0]]}</td><td>${row.rates[selectedCurrencies[1]]}</td></tr>`;
-        }).join('');
-        const tableHTML = tableHeader + '<table><thead>' + tHeadMarkup + '</thead><tbody>' + tBodyMarkup + '</tbody></table>';
-        modalBody.innerHTML = tableHTML;
-    };
-
-
-    /**
-     * Handle form submit events
-     */
-    const submitHandler = (e) => {
-        e.preventDefault();
-        
-        if (!amountInput) return;
-        
-        newBaseValue = amountInput.value;
-
-        if (!newBaseValue || newBaseValue.length === 0) return;
-
-        baseValue = newBaseValue;
-
-        updateLatestRatesTable(newBaseValue);
-    };
-
-    /**
-     * Handle click events (specifically selecting currency rows)
-     */
-    const clickHandler = (event) => {
-
-        // Currency row clicks
-        if (event.target.matches('td')) {
-            const currencyRow = event.target.parentNode;
-            selectCurrencyForComparison(currencyRow);
-            return;
-        }
-
-        // 'Close modal' clicks
-        if (event.target.matches('#close-button')) {
-            clearSelectedCurrencies();
-            modal.classList.toggle("closed");
-            modalOverlay.classList.toggle("closed");
-            return;
-        }
-        
-    };
-
-    // Listen for events
-    
-    document.addEventListener('submit', submitHandler, false);
-    document.addEventListener('click', clickHandler, false);
 
 })();
